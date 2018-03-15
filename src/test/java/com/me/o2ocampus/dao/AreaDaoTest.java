64f3498fa613816152296afd4f3b6f1712ec90d6
SavorTheFlavor <2500364530@qq.com>
2018-03-15 14:18:46 +0800

    logback and test
    logback major tags:
    logger
    appender
    layout

--- src/test/java/com/me/o2ocampus/dao/AreaDaoTest.java
+++ src/test/java/com/me/o2ocampus/dao/AreaDaoTest.java
@@ -1,17 +1,23 @@
 package com.me.o2ocampus.dao;
 
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.me.o2ocampus.BaseTest;
 import com.me.o2ocampus.entity.Area;
 
+
 public class AreaDaoTest extends BaseTest{
 	@Autowired
 	private AreaDao areaDao;
 	
+	private Logger logger = LoggerFactory.getLogger(AreaDaoTest.class);
+	
 	@Test
 	public void test1() {
+		logger.debug("testing AreaDao......");
 		Area area = areaDao.findOne();
 		System.out.println(area.getAreaName());
 	}
